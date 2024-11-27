751ffaa9295668904e0f9f86e2e2b53396d0a9e2
Md Ismail Hossain <121042412+Ismail0817@users.noreply.github.com>
2024-11-27 14:33:48 +0100

    practice done

---
+++ first-react/src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {   
+    return (
+      <footer>
+        &copy; 2024 Hossain development. All rights reserved.
+      </footer>
+    )
+  }
+
+export default Footer
--- first-react/src/main.jsx
+++ first-react/src/main.jsx
@@ -11,41 +11,14 @@
 
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Header from './Header'
+import Maincontent from './MainContent'
+import Footer from './Footer'
 
 const root = createRoot(document.getElementById('root'))
 root.render(
   <Page />
   )
-
-  
-function Header() {
-  return (
-    <header>
-      <img src='src/assets/react.svg' alt='logo' width={40}/>
-    </header>
-  )
-}
-
-function Maincontent(){
-  return (
-    <main>
-      <h1>Why I am interested in React</h1>
-      <ol>
-        <li>It is a popular library</li>
-        <li>It is easy to use</li>
-        <li>It is a powerful library</li>
-      </ol>
-    </main>
-  )
-}
-
-function Footer() {   
-  return (
-    <footer>
-      &copy; 2024 Hossain development. All rights reserved.
-    </footer>
-  )
-}
 
 function Page() {
   return (
